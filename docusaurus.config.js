fcb075c0053198f67849981976750a25227b7e1e
sksk713 <sskk713@ajou.ac.kr>
2021-01-04 16:39:37 +0900

    Done

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,13 +88,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/sksk713/TIL/edit/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          "https://github.com/sksk713/TIL/edit/master/",
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
